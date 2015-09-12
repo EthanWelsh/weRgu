8824b36852bee4adfc8b445353138339941c78d3
Ritwik Gupta <ritwikg2004@live.com>
2015-09-12 02:36:32 -0400

    Added tweepy stuff

--- wergu/views.py
+++ wergu/views.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template
 
+from secrets import consumer_key, consumer_secret, access_token, access_token_secret
+
+import tweepy
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
 wergu = Flask(__name__)
 
 
@@ -7,6 +16,9 @@ wergu = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@wergu.route('/tweets', strict_slashes=False)
+def tweets():
+    
 
 if __name__ == "__main__":
     wergu.run(debug=True)
