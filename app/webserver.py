82e6435dec0cb40bf3bbf94f7270f6fddb189515
Ethan Welsh <ejw45@pitt.edu>
2015-09-12 19:19:10 -0400

    almost there... I can see the light

--- app/webserver.py
+++ app/webserver.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template
 
 from twitter.api import get_subject_tweets
-
+import bayesian_analysis.sentiment
 
 wergu = Flask(__name__)
+nbc = None
 
 
 @wergu.route('/')
 def index():
-    return render_template('app/templates/index.html')
+    return render_template('index.html')
 
 
 @wergu.route('/tweets/<subject>', strict_slashes=False)
@@ -16,25 +17,26 @@ def tweets(subject):
     return str(get_subject_tweets(subject))
 
 
-@wergu.route('/procon')
-def pro_con():
-    issue_title = "#Trump2016"
-    pro_list = [
-        "Watching @realDonaldTrump on @jimmyfallon!!!!  Can't wait to say President Trump #Trump2016 #trumpnation @WeSupportTrump @trump2016fan",
-        "Mr @realDonaldTrump just wanted you to know you have a huge following in Canada...They want you to win in #Trump2016 @DanScavino #TeamTrump",
-        "@realDonaldTrump Genius #Trump2016 Just do it #MakeAmericaGreatAgain!!!! @realDonaldTrump @FallonTonight",
-        "@realDonaldTrump you were amazing on @jimmyfallon @FallonTonight !! #Trump2016 very exciting!  Presidential & truly a joy to watch!"
-    ]
+@wergu.route('/procon/<subject>')
+def pro_con(subject):
+    issue_title = subject
 
-    con_list = [
-        "Every day, this waking nightmare of #donaldtrump ascending the polls truly makes me wonder if I'm in a twisted version of the Truman Show.",
-        "DonaldTrump just another idiot silver spoon trust fund baby lk #GWBush avg Conservative 'Rich Guy Groupies' vote 4",
-        "#DonaldTrump has ZERO military experience, but he's on record saying he knows more than many in the military or veterans? Just... wow.",
-        "I can't believe that people are taking #donaldtrump seriously. He's the worst sort of idiot: one with an ego. He'll get us all killed."
-    ]
+    list_of_tweets = get_subject_tweets(subject)
+    con_tweets = []
+    pro_tweets = []
 
-    return render_template('app/templates/procon.html', issue_title=issue_title, pro_list=pro_list, con_list=con_list)
+    for tweet in list_of_tweets:
+        sentiment_value = bayesian_analysis.sentiment.get_tweet_sentiment(tweet)
+        if sentiment_value < -.3:
+            con_tweets.append(tweet)
+        elif sentiment_value > .3:
+            pro_tweets.append(tweet)
+
+    return render_template('procon.html', issue_title=issue_title, pro_list=pro_tweets, con_list=con_tweets)
 
 
-def start():
-    wergu.run(debug=True)
+def start(naive_bayes_classifier):
+    global nbc
+
+    wergu.run()
+    nbc = naive_bayes_classifier
--- bayesian_analysis/sentiment.py
+++ bayesian_analysis/sentiment.py
@@ -1,23 +1,29 @@
-def get_sentence_sentiment(sentence):
+def get_subject(subject, nbc):
+    """
+    :param subject: The subject/issue that you are interested in seeing
+    :return: positive: a list of tweets that embody a positive sentiment towards the given subject area.
+    :return: negative: a list of tweets that embody a negative sentiment towards the given subject area.
+    """
+
+def get_tweet_sentiment(sentence, nbc):
     """Given a string, will split that string into several words, analyze them using naive base, and then output the
     probability from -1.0 to 1.0 that the string in question in negative/positive in mood and nature.
 
     :param sentence: The sentence that you will be measuring.
     :return: sentiment: The sentiment grade that shows how positive or negative a sentence is.
-    :return: probability: The level of precision behind the bayesian forecast
     """
 
     words = sentence.split(' ')
     sentiments = [get_word_sentiment(word) for word in words]
-    return sum(sentiments[0]) / float(len(words)), sum(sentiments[1]) / float(len(words))
+    return sum(sentiments) / float(len(words))
 
 
-def get_word_sentiment(word):
+def get_word_sentiment(word, nbc):
     """Given a single word, will analyze said word using naive base, and then output the
     probability from -1.0 to 1.0 that the word in question in negative/positive in mood and nature.
 
     :param word: The word that you will be measuring.
     :return: sentiment: The sentiment grade that shows how positive or negative a word is.
-    :return: probability: The level of precision behind the bayesian forecast
     """
-    pass
+
+    return nbc.classify({word:True})
