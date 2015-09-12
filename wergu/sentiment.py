b995352c46d5141306c87ed349bec9dcd08d72c4
Ethan Welsh <ejw45@pitt.edu>
2015-09-12 13:08:12 -0400

    add template for baynesian sentiment analysis

---
+++ wergu/sentiment.py
@@ -0,0 +1,22 @@
+def get_sentence_sentiment(sentence):
+    """Given a string, will split that string into several words, analyze them using naive base, and then output the
+    probability from -1.0 to 1.0 that the string in question in negative/positive in mood and nature.
+
+    :param sentence: The sentence that you will be measuring.
+    :return: sentiment: The sentiment grade that shows how positive or negative a sentence is.
+    :return: probability: The level of precision behind the bayesian forecast
+    """
+
+    pass
+
+
+def get_word_sentiment(word):
+    """Given a single word, will analyze said word using naive base, and then output the
+    probability from -1.0 to 1.0 that the word in question in negative/positive in mood and nature.
+
+    :param word: The word that you will be measuring.
+    :return: sentiment: The sentiment grade that shows how positive or negative a word is.
+    :return: probability: The level of precision behind the bayesian forecast
+    """
+    pass
+
